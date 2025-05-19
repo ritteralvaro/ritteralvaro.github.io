53dc5eba2bca1cee71b48112db6c0783b5db1180
ritteralvaro <ritteralvaro@gmail.com>
2025-05-18 21:05:07 -0300

    colocando script de startup

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,18 @@ import "./globals.css"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import FramerAnimations from "@/components/framer-animations"
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Alvaro Ritter - Design, Marketing & Growth",
   description:
-    "Designer com ampla experiência em soluções personalizadas para UX/UI, Growth Marketing e Design Gráfico",
+    "Designer com ampla experiência em soluções personalizadas para UX/UI, Growth Marketing e Design",
+  // Adicione a meta tag aqui
+  other: {
+    'everleads-secret': 'eyJpdiI6InZ2NTBmTDh0Rm8vKzdsbnFhM1dlRGc9PSIsInZhbHVlIjoiZ2pVa3NzTEh5Ynhqd0NzNzB4K3hHdz09IiwibWFjIjoiZDk1NGEzMzgwNzg3N2RhZjU1MDY4OGE2OWVlY2E2ZDg1ZTFlZjhmMGZiMzBjNDQ5YjI5YzNhYzliYTQ4MzhmMCIsInRhZyI6IiJ9'
+  }
 }
 
 export default function RootLayout({
@@ -22,6 +27,10 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
+        <Script
+          src="https://everleads.clusterlabs.com.br/lib/everleads.js"
+          strategy="afterInteractive"
+        />
         <Header />
         <main>{children}</main>
         <Footer />
