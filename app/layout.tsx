c75e000b6f4c5882ef642046ba673c338207d297
ritteralvaro <ritteralvaro@gmail.com>
2025-05-29 04:10:00 -0300

    adição everleads!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -6,6 +8,7 @@ import Header from "@/components/header"
 import Footer from "@/components/footer"
 import FramerAnimations from "@/components/framer-animations"
 import Script from 'next/script'
+import { useEffect } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,18 +16,31 @@ export const metadata: Metadata = {
   title: "Alvaro Ritter - Design, Marketing & Growth",
   description:
     "Designer com ampla experiência em soluções personalizadas para UX/UI, Growth Marketing e Design",
-  // Adicione a meta tag aqui
-  other: {
-    'everleads-secret': 'eyJpdiI6Ik4zTytGR3FGdkpjVDU1dnU0dmhReXc9PSIsInZhbHVlIjoiOXp4eEJacE1XQXMydmZqdGJaUk5KZz09IiwibWFjIjoiZDRhNDUxZWNhNDlmOWJkZjcwNWM5MGJhZTBmOWY3YTllZjZhZGM4YzJlYzkxNzJmNTdmODQyYjZlYjlmMWVhYSIsInRhZyI6IiJ9'
-  }}
+  }
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+      useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://everleads.clusterlabs.com.br/lib/everleads.js';
+        script.async = true;
+        script.onload = () => {
+            window.EverLeads.init()
+        };
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script); // limpa ao desmontar o componente
+        };
+    }, []);
+
   return (
     <html lang="pt-BR">
+      <meta name="everleads-eid" content="l3mE90X9a1pD"/>
       <body className={inter.className}>
         <Script
           src="https://everleads.clusterlabs.com.br/lib/everleads.js"
